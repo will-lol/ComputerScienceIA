ab5624b1673044aff2ae6faa8cb3de51c1cfcee2
William Bradshaw <will.bradshaw50@gmail.com>
2023-04-30 19:27:15 +1000

    window resizing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,9 +5,11 @@
 	import noiseWorkerCreator from './noiseWorker?worker';
 
 	let canvas: HTMLCanvasElement;
+	let ctx: CanvasRenderingContext2D;
 	let noiseOverlay: HTMLCanvasElement;
 	let simplexWorker: Worker;
 	let noiseWorker: Worker;
+	let animationController = new AbortController();
 	const onServer = typeof process === 'object';
 	if (!onServer) {
 		simplexWorker = new simplexWorkerCreator();
@@ -41,58 +43,87 @@
 		}
 
 		//generate simplex noise and animate
-		const ctx = canvas.getContext('2d')!;
-		canvas.height = boundingClientRect.height / 20;
-		canvas.width = boundingClientRect.width / 20;
-
+		ctx = canvas.getContext('2d')!;
 		const reducedMotionQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
-		let isReducedMotion = reducedMotionQuery.matches;
-		reducedMotionQuery.addEventListener('change', () => {
-			isReducedMotion = reducedMotionQuery.matches;
-			if (isReducedMotion) {
-				frame(canvas.width, canvas.height, 0, 0);
+		reducedMotionQuery.addEventListener('change', async () => {
+			if (reducedMotionQuery.matches) {
+				animationController.abort();
+				renderSimplexNoise(ctx, animationController);
 			}
 		});
 
 		if (!onServer) {
-			await frame(canvas.width, canvas.height, 0, 0);
+			await renderSimplexNoise(ctx, animationController);
 			fadeIn(canvas);
 		}
 
 		function fadeIn(elem: HTMLElement) {
 			elem.animate([{ opacity: 0 }, { opacity: 1 }], { duration: 1000, fill: 'forwards' });
 		}
+	});
 
-		async function frame(width: number, height: number, timeAxis: number, count: number) {
-			if (count == 10) {
-				const imgData: Promise<Uint8ClampedArray> = new Promise((resolve, reject) => {
-					simplexWorker.postMessage({
-						width: canvas.width,
-						height: canvas.height,
-						timeAxis: timeAxis
-					});
-					simplexWorker.onmessage = (e) => {
-						resolve(e.data as Uint8ClampedArray);
-					};
-					simplexWorker.onmessageerror = (e) => {
-						reject();
-					};
-				});
+	async function renderSimplexNoise(ctx: CanvasRenderingContext2D, controller: AbortController) {
+		const boundingClientRect = canvas.getBoundingClientRect();
+		canvas.height = boundingClientRect.height / 20;
+		canvas.width = boundingClientRect.width / 20;
 
-				const img = new ImageData(await imgData, width, height);
-
-				timeAxis = timeAxis + 0.03;
-				ctx.putImageData(img, 0, 0);
-				count = 0;
-			}
-			if (!isReducedMotion) {
-				count++;
-				requestAnimationFrame(() => frame(width, height, timeAxis, count));
+		if (!controller.signal.aborted) {
+			const reducedMotionQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
+			if (reducedMotionQuery.matches) {
+				controller.abort();
 			}
 		}
-	});
+
+		await frame(ctx, canvas.width, canvas.height, 0, 0, controller.signal);
+	}
+
+	let timeoutID: number;
+	function resize() {
+		clearTimeout(timeoutID);
+		timeoutID = setTimeout(() => {
+			animationController.abort();
+			animationController = new AbortController();
+			renderSimplexNoise(ctx, animationController);
+		}, 500)
+	}
+
+	async function frame(
+		ctx: CanvasRenderingContext2D,
+		width: number,
+		height: number,
+		timeAxis: number,
+		count: number,
+		signal: AbortSignal
+	) {
+		if (count == 10 || signal.aborted) {
+			const imgData: Promise<Uint8ClampedArray> = new Promise((resolve, reject) => {
+				simplexWorker.postMessage({
+					width: canvas.width,
+					height: canvas.height,
+					timeAxis: timeAxis
+				});
+				simplexWorker.onmessage = (e) => {
+					resolve(e.data as Uint8ClampedArray);
+				};
+				simplexWorker.onmessageerror = (e) => {
+					reject();
+				};
+			});
+
+			const img = new ImageData(await imgData, width, height);
+
+			timeAxis = timeAxis + 0.03;
+			ctx.putImageData(img, 0, 0);
+			count = 0;
+		}
+		if (!signal.aborted) {
+			count++;
+			requestAnimationFrame(() => frame(ctx, width, height, timeAxis, count, signal));
+		}
+	}
 </script>
 
+<svelte:window on:resize={resize} />
 <canvas bind:this={canvas} class="opacity-0 fixed w-screen h-screen pointer-events-none -z-10" />
 <canvas
 	bind:this={noiseOverlay}
