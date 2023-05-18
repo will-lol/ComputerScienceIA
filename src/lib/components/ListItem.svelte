fc9b53b3f45fe9e809885b9106d4c067d36a6b48
William Bradshaw <will.bradshaw50@gmail.com>
2023-05-18 19:27:49 +1000

    modify typeface

--- src/lib/components/ListItem.svelte
+++ src/lib/components/ListItem.svelte
@@ -4,7 +4,7 @@ class="[counter-increment:list] mb-2 bg-white py-2 pl-3 pr-4 border border-solid
 <div
     class="mr-2 w-6 h-6 bg-primary bg-blue-300 bg-opacity-50 border border-solid border-blue-300 rounded-full items-center inline-flex"
 >
-    <span class="text-sm mx-auto before:content-[counter(list)]"></span>
+    <span class="text-sm font-mono mx-auto before:content-[counter(list)]"></span>
 </div>
 <slot></slot>
 </li>
