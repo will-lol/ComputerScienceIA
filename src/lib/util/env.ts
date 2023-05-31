0734b212e80da68d462854edefee57dadd66b313
William Bradshaw <will.bradshaw50@gmail.com>
2023-05-31 21:54:14 +1000

    remove that pesky env

--- src/lib/util/env.ts
+++ src/lib/util/env.ts
@@ -1,13 +1,6 @@
-import dotenv from 'dotenv'
-
 type envObj = {
     CLIENT_ID: string,
     CLIENT_SECRET: string
 }
 
-const env = dotenv.config().parsed;
-if (env == undefined) {
-    throw "no env"
-}
-
-export default env as envObj;
+export default process.env as envObj;
