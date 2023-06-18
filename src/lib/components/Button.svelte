698c7af6fef7c15e550bc0b44845952f873c26c8
William Bradshaw <will.bradshaw50@gmail.com>
2023-06-18 13:03:02 +1000

    home page redesign

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -3,7 +3,9 @@
 	export let absoluteBottom: boolean = false;
 	export let primary: boolean = false;
 	export let transparent: boolean = false;
-	let classes: string[] = ['h-10 py-2 px-4 inline-flex items-center justify-center rounded text-sm font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background'];
+	export let small: boolean = false;
+	export let destructive: boolean = false;
+	let classes: string[] = ['inline-flex items-center justify-center rounded font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background'];
 	if (fullWidth) {
 		classes.push('w-full');
 	}
@@ -12,12 +14,19 @@
 	} else {
 		classes.push('relative')
 	}
+	if (small) {
+		classes.push('py-0 px-2 text-xs h-8')
+	} else {
+		classes.push('py-2 px-4 text-sm h-10')
+	}
 	if (primary) {
 		classes.push('bg-blue-500 hover:bg-blue-600 text-white shadow');
 	} else if (transparent) {
 		classes.push('bg-transparent underline')
+	} else if (destructive) {
+		classes.push('bg-red-500 hover:bg-red-600 text-white border border-solid border-red-600 shadow')
 	} else {
-		classes.push('bg-gray-50 bg-opacity-75 hover:bg-gray-100 shadow');
+		classes.push('bg-gray-50 bg-opacity-75 border border-solid border-gray-200 hover:bg-gray-100 shadow');
 	}
 </script>
 
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -53,8 +53,8 @@
 		} catch {
 			buttonState = 'Parser error. Try again.';
 			setTimeout(() => {
-				buttonState = 'Add an entry'
-			}, 5000)
+				buttonState = 'Add an entry';
+			}, 5000);
 			return;
 		}
 
@@ -74,16 +74,6 @@
 				buttonState = 'Add an entry';
 			}, 5000);
 		}
-	}
-
-	let last: HTMLInputElement | null;
-	function checkInput(event: Event & { currentTarget: EventTarget & HTMLInputElement }) {
-		if (entryCheckboxes.length > 2) {
-			const currentVal = parseInt(event.currentTarget.value);
-			const lastVal = parseInt(last!.value);
-			entryCheckboxes = [currentVal, 3 - (lastVal + currentVal)];
-		}
-		last = event.currentTarget;
 	}
 
 	async function fetchStatistics(s3Key: string) {
@@ -150,29 +140,32 @@
 	}
 </script>
 
-<div class="">
-	<form on:submit|preventDefault={submitHandler}>
-		<input id="file" accept=".xml" type="file" class="hidden" bind:files />
-		<fieldset class="flex flex-col border border-solid border-black rounded p-4">
-			<legend>Choose a saved entry to view it, or choose two to compare between.</legend>
-			{#if loading}
-				<div>Loading<Ellipses /></div>
-			{/if}
+<form on:submit|preventDefault={submitHandler}>
+	<input id="file" accept=".xml" type="file" class="hidden" bind:files />
+	<fieldset class="flex flex-col gap-3 bg-white border border-solid border-gray-300 shadow rounded p-4">
+		<h2 class="text-sm text-gray-600">Choose a saved entry to view it, or choose two to compare between.</h2>
+		{#if loading}
+			<div>Loading<Ellipses /></div>
+		{/if}
+		<div class="flex flex-col gap-3">
 			{#each allData as data, i}
 				<div class="flex flex-row justify-between">
-					<div>
+					<div class="flex items-center">
 						<input
 							bind:group={entryCheckboxes}
 							type="checkbox"
-							name={data.date.toLocaleDateString()}
+							class="w-5 h-5 mr-2"
+							name={data.date.toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' })}
 							id={data.date.toString()}
 							value={i}
-							on:change={checkInput}
+							disabled={!entryCheckboxes.includes(i) && entryCheckboxes.length == 2}
 						/>
-						<label for={data.date.toString()}>{data.date.toLocaleString()}</label>
+						<label class="" for={data.date.toString()}>{data.date.toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' })}</label>
 					</div>
-					<button
+					<Button
 						type="button"
+						destructive
+						small
 						on:click={async () => {
 							let deleteUrl = new URL(globalThis.location.origin + '/api/deleteData');
 							deleteUrl.searchParams.set('id', data.id.toString());
@@ -181,32 +174,31 @@
 								refreshList();
 							}
 						}}
-						class="disabled:opacity-40 disabled:pointer-events-none">Delete</button
-					>
+					>Delete</Button>
 				</div>
 			{/each}
+		</div>
 
-			<Button type="button" disabled={buttonState != 'Add an entry'}
-				><label
-					for="file"
-					class="cursor-pointer absolute top-0 left-0 w-full h-full flex justify-center items-center"
-				>
-					{buttonState}
-					{#if !(buttonState == 'Add an entry' || buttonState == 'Parser error. Try again.')}
-						<Ellipses />
-					{/if}
-				</label></Button
+		<Button type="button" disabled={buttonState != 'Add an entry'}
+			><label
+				for="file"
+				class="cursor-pointer absolute top-0 left-0 w-full h-full flex justify-center items-center"
 			>
-			<Button disabled={entryCheckboxes.length == 0 || submitState != 'View'} primary type="submit">
-				{submitState}
-				{#if !(submitState == 'View' || submitState == 'Select at least one checkbox' || submitState == 'Statistics sent by server are unexpected.')}
+				{buttonState}
+				{#if !(buttonState == 'Add an entry' || buttonState == 'Parser error. Try again.')}
 					<Ellipses />
-				{:else if submitState == 'View'}
-					{#if entryCheckboxes.length == 2} 
-					{timeElapsedString(allData[entryCheckboxes[0]].date, allData[entryCheckboxes[1]].date)}
-					{/if}
 				{/if}
-			</Button>
-		</fieldset>
-	</form>
-</div>
+			</label></Button
+		>
+		<Button disabled={entryCheckboxes.length == 0 || submitState != 'View'} primary type="submit">
+			{submitState}
+			{#if !(submitState == 'View' || submitState == 'Select at least one checkbox' || submitState == 'Statistics sent by server are unexpected.')}
+				<Ellipses />
+			{:else if submitState == 'View'}
+				{#if entryCheckboxes.length == 2}
+					{timeElapsedString(allData[entryCheckboxes[0]].date, allData[entryCheckboxes[1]].date)}
+				{/if}
+			{/if}
+		</Button>
+	</fieldset>
+</form>
