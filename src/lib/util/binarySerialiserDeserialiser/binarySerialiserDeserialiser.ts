4f2c1fdca058a03638f2cfa39907a0a653bc7d7a
William Bradshaw <will.bradshaw50@gmail.com>
2023-06-20 12:05:48 +1000

    USE HTE TPOROGVUF

---
+++ src/lib/util/binarySerialiserDeserialiser/binarySerialiserDeserialiser.ts
@@ -0,0 +1,92 @@
+import type { dataPackage } from '../zod';
+import * as protobuf from 'protobufjs';
+
+const jsonProtoDef = {
+	nested: {
+		main: {
+			nested: {
+				dataPackage: {
+					fields: {
+						metadata: {
+							type: 'metadata',
+							id: 1
+						},
+						songs: {
+							rule: 'repeated',
+							type: 'song',
+							id: 2
+						}
+					}
+				},
+				song: {
+					fields: {
+						name: {
+                            rule: 'optional',
+							type: 'string',
+							id: 1
+						},
+						artist: {
+                            rule: 'optional',
+							type: 'string',
+							id: 2
+						},
+						album: {
+                            rule: 'optional',
+							type: 'string',
+							id: 3
+						},
+						genre: {
+                            rule: 'optional',
+							type: 'string',
+							id: 4
+						},
+						time: {
+                            rule: 'optional',
+							type: 'int32',
+							id: 5
+						},
+						play_count: {
+                            rule: 'optional',
+							type: 'int32',
+							id: 6
+						},
+						skip_count: {
+                            rule: 'optional',
+							type: 'int32',
+							id: 7
+						},
+						rating: {
+                            rule: 'optional',
+							type: 'int32',
+							id: 8
+						}
+					}
+				},
+                metadata: {
+                    fields: {
+                        date: {
+                            type: 'uint64',
+                            id: 1
+                        }
+                    }
+                }
+			},
+		}
+	}
+};
+
+const dataPackageParser = protobuf.Root.fromJSON(jsonProtoDef).lookupType("main.dataPackage");
+
+export function toBinary(input: dataPackage): Uint8Array {
+    console.log(input);
+    const transformed: any = input;
+    transformed.metadata.date = input.metadata.date.valueOf();
+    console.log(input.metadata.date.valueOf())
+	return dataPackageParser.encode(dataPackageParser.fromObject(transformed)).finish();
+}
+
+export function fromBinary(input: Uint8Array): dataPackage {
+    const transformed = dataPackageParser.decode(input).toJSON();
+    transformed.metadata.date = new Date(parseInt(transformed.metadata.date));
+	return transformed as dataPackage;
+}
