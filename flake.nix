c11a53868201aa36272f0ef969d7aab199e05c1b
Will <will.bradshaw50@gmail.com>
2024-01-20 16:43:09 +1100

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "ComputerScienceIA";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, templRepo }:
+    flake-utils.lib.eachDefaultSystem (system: 
+      let 
+	overlays = [];
+	lib = nixpkgs.lib;
+	manifest = (lib.importTOML ./Cargo.toml).package;
+	pkgs = import nixpkgs { inherit system overlays; };
+      in
+        {
+	  packages = {
+	  };
+	  devShell = pkgs.mkShell {
+	    packages = [ pkgs.nodejs_18 ];
+	  };
+	}
+    );
+}
